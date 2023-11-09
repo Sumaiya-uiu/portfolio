6ca86fff4fd6ee34505e86e5023cbac390359652
Sumaiya <jahansumaiya49@gmail.com>
2023-11-10 00:05:01 +0600

    partial portfolio

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i>A Recent CS Graduate</i>', '<i>Frontend Developer</i>', '<i>Problem solver</i>',],
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 3000,
+    backSpeed: 30,
+});
